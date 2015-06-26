711bc073144b9617c19746c2b4e7c140c3412834
Tom Petty <hello@anothertompetty.com>
2015-06-26 10:00:35 +0100

    adding x icon when menu open

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -2,6 +2,8 @@ $(document).on('ready', function() {
 
   $('.menu__button').on('click', function() {
     $('.menu').fadeToggle(125);
+    $('.menu__button__icon').fadeToggle(125);
+    $('.menu__button__icon--close').fadeToggle(125);
   });
 
 });
