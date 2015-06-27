a00ff162be12ec963f9c3d974a65415854c41d37
Tom Petty <hello@anothertompetty.com>
2015-06-27 12:37:47 +0100

    replacing fadeToggle w toggle

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -2,8 +2,8 @@ $(document).on('ready', function() {
 
   $('.menu__button').on('click', function() {
     $('.menu').fadeToggle(125);
-    $('.menu__button__icon').fadeToggle(125);
-    $('.menu__button__icon--close').fadeToggle(125);
+    $('.menu__button__icon').toggle();
+    $('.menu__button__icon--close').toggle();
   });
 
 });
